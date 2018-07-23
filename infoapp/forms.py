4daaa8b733d9f73597386eda5ad8ebece328c243
abdulhafeez1432 <abdulhafeez1432@gmail.com>
2018-07-23 18:10:54 +0100

    Last uploading

--- infoapp/forms.py
+++ infoapp/forms.py
@@ -4,16 +4,9 @@ from django.core.exceptions import ValidationError
 
 
 class PersonalInfoForm(forms.ModelForm):
-	fullname = forms.CharField(help_text="Enter Your Fullname")
-	email = forms.CharField(help_text="Enter Your Email")
+    surname = forms.CharField(help_text="Enter Your Fullname")
+    email = forms.CharField(help_text="Enter Your Email")
 
-	class Meta:
-		model = PersonalData
-		fields = ['fullname', 'email']
-
-	def clean(self):
-		cleaned_data = super(PersonalInfoForm, self).clean()
-		fullname = cleaned_data.get('fullname')
-		email = cleaned_data.get('email')
-		if not fullname and not email:
-			raise forms.ValidationError("Please, Enter Your Details before Submission")
+    class Meta:
+        model = PersonalData
+	fields = ['surname', 'othername', 'email', 'phone', 'state', 'address', ]    
--- infoapp/models.py
+++ infoapp/models.py
@@ -3,12 +3,21 @@ from django.db import models
 # Create your models here.
 
 class PersonalData(models.Model):
-	fullname = models.CharField(max_length=264)
-	email = models.EmailField(max_length=254, unique=True)
+  
 
-	def __str__(self):
-		return self.fullname
+    
+    surname = models.CharField(max_length=264)
+    othername = models.CharField(max_length=264)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    state = models.CharField(max_length=100, blank=True)
+    address = models.TextField(max_length=500, blank=True)   
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		verbose_name = "info"
-		verbose_name_plural = "infos"
+    def __str__(self):
+        return self.surname
+
+    class Meta:
+        verbose_name = "info"
+	verbose_name_plural = "infos"
